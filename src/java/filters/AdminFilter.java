a0396b848c903ebd3bc6a6ae7923060396324a8f
686623 <686623@ELF6IT215949.ACDM.DS.SAIT.CA>
2017-11-21 10:40:23 -0700

    Created Week 10 Lab Added Filter An admin can now go to users url if its not an admin it will redirect them to homepage

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -5,7 +5,11 @@
  */
 package filters;
 
+import businesslogic.UserService;
+import domainmodel.User;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -20,41 +24,49 @@ import javax.servlet.http.HttpSession;
  *
  * @author awarsyle
  */
-public class AuthenticationFilter implements Filter {
-    
+public class AdminFilter implements Filter {
+
     private FilterConfig filterConfig = null;
-    
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
             FilterChain chain)
             throws IOException, ServletException {
-        
+
         // this code executes before the servlet
         // ...
-        
         // ensure user is authenticated
-        HttpSession session = ((HttpServletRequest)request).getSession();
-        if (session.getAttribute("username") != null) {
-            // yes, go onwards to the servlet or next filter
-            chain.doFilter(request, response);
-        } else {
-            // get out of here!
-            ((HttpServletResponse)response).sendRedirect("login");
-        }
-        
-       // this code executes after the servlet
-       // ...
+        HttpSession session = ((HttpServletRequest) request).getSession();
+        UserService us = new UserService();
+        String uName = (String) session.getAttribute("username");
+        User user;
+
+        try {
+            user = us.get(uName);
             
+            if (user.getRole().getRoleID().equals(1)) {
+                // yes, go onwards to the servlet or next filter
+                chain.doFilter(request, response);
+            } else {
+                // get out of here!
+                ((HttpServletResponse) response).sendRedirect("home");
+            }
+
+            // this code executes after the servlet
+            // ...
+        } catch (Exception ex) {
+            Logger.getLogger(AdminFilter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 
     @Override
-    public void destroy() {        
+    public void destroy() {
     }
 
     @Override
-    public void init(FilterConfig filterConfig) {        
+    public void init(FilterConfig filterConfig) {
         this.filterConfig = filterConfig;
     }
 
-    
 }
